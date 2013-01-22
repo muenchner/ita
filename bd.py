5c0300da5640fe01afc7861d3d0571abd6ac369b
Mahalia Miller <mahalia@alum.mit.edu>
2013-01-21 23:38:16 -0800

    more

--- bd.py
+++ bd.py
@@ -19,7 +19,7 @@ def build_demand(trip_filename, centroid_filename):
       print row
       tokens = row.split(',')
       print tokens
-      demand_dict[int(tokens[0])][int(tokens[1])] = int(tokens[12])
+      demand_dict[sd_dict[int(tokens[0])]][sd_dict[int(tokens[1])]] = int(tokens[12])
   return demand_dict
 
 
